4d82d18c079ad1ec37dfb5ee89e927850efa1f64
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-10-17 18:57:45 +0100

    Added the new domain to app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ import requestRouter from "./routes/request.routes.js";
 const app = express();
 
 app.use(cors({
-    origin : ["http://localhost:5173"],
+    origin : ["http://localhost:5173", "https://mobilia-5358.vercel.app"],
     credentials : true
 }));
 
